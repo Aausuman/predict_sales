250ab439d8817e6633d3c021ec7a1407d45cb0d7
Aausuman Deep <aausuman.deep@gmail.com>
2022-02-03 23:40:09 +0000

    Initial Analysis of the training dataset

--- Analysis_Script.R
+++ Analysis_Script.R
@@ -1,3 +1,8 @@
 sales_data = read.csv('~/Documents/Gits/predict_sales/data/sales_train.csv',header = TRUE)
 
-sales data
+no_of_items = nrow(unique(sales_data[c('item_id')]))
+# 21807 distinct items in total.
+
+no_of_shops = nrow(unique(sales_data[c('shop_id')]))
+# 60 shops in total
+
