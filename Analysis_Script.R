abe590c4bdc982a58c2ea0dde1cea67e8f859136
Aausuman Deep <aausuman.deep@gmail.com>
2022-02-03 23:56:53 +0000

    Updates to Initial Analysis.

--- Analysis_Script.R
+++ Analysis_Script.R
@@ -1,8 +1,11 @@
 sales_data = read.csv('~/Documents/Gits/predict_sales/data/sales_train.csv',header = TRUE)
+shops_data = read.csv('~/Documents/Gits/predict_sales/data/shops.csv', header = TRUE)
+items_data = read.csv('~/Documents/Gits/predict_sales/data/items.csv', header = TRUE)
+items_categories_data = read.csv('~/Documents/Gits/predict_sales/data/item_categories.csv', header = TRUE)
 
-no_of_items = nrow(unique(sales_data[c('item_id')]))
-# 21807 distinct items in total.
+nrow(items_data)
+# 22170 items in total on the market
 
-no_of_shops = nrow(unique(sales_data[c('shop_id')]))
-# 60 shops in total
+no_of_items_sold = nrow(unique(sales_data[c('item_id')]))
+# 21807 out of total 22170 items have been sold.
 
